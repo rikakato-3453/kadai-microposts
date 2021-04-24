1902231bb5ea0dff1e588aa561eb6497c044f52f
r.k <rikakato215c0169@gmail.com>
2021-04-24 14:20:11 +0000

    dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-  has_many :favorites
-  has_many :favoritings, through: :favorites, source: :micropost 
+  has_many :favorites, dependent: :destroy
+  has_many :favoritings, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
